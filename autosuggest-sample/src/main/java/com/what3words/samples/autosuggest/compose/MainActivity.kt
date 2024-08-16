092bc181bd2e3f8e63f273738dc2302abe2470a9
Mani <emanuel@what3words.com>
2024-08-16 17:03:45 +0100

    - updated autosuggest-sample and autosuggest-voice-sample to targetSDK 34. - updated to material3 compose

--- autosuggest-sample/src/main/java/com/what3words/samples/autosuggest/compose/MainActivity.kt
+++ autosuggest-sample/src/main/java/com/what3words/samples/autosuggest/compose/MainActivity.kt
@@ -3,20 +3,22 @@ package com.what3words.samples.autosuggest.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import com.what3words.samples.autosuggest.compose.ui.screen.W3WTextFieldInConstraintLayoutScreen
-import com.what3words.samples.autosuggest.compose.ui.theme.What3WordsComponentsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            What3WordsComponentsTheme {
+            MaterialTheme(
+                colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+            ) {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
+                Surface {
                     W3WTextFieldInConstraintLayoutScreen()
                 }
             }
