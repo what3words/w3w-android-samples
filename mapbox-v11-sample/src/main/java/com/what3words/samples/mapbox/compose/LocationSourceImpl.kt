381df6d6dcdba86dd6d793568aafa5f435a83d82
kaytran2992 <kay.kiet@what3words.com>
2024-11-20 15:10:32 +0700

    Update sample for location source

---
+++ mapbox-v11-sample/src/main/java/com/what3words/samples/mapbox/compose/LocationSourceImpl.kt
@@ -0,0 +1,25 @@
+package com.what3words.samples.mapbox.compose
+
+import android.Manifest
+import android.content.Context
+import android.location.Location
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import com.what3words.components.compose.maps.models.W3WLocationSource
+
+class LocationSourceImpl(private val context: Context): W3WLocationSource {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    override fun fetchLocation(
+        onLocationFetched: (Location) -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    onLocationFetched(it)
+                }
+            }
+        }
+    }
+}
